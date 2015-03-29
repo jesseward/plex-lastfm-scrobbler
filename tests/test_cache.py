f6854eaf0c980b9cd1d0cd59f1d9ea7d19cda4bd
Jesse Ward <jesse.ward@gmail.com>
2015-03-28 21:45:51 -0700

    Cleaning up tests/test_*.py.

--- tests/test_cache.py
+++ tests/test_cache.py
@@ -8,14 +8,15 @@ from plex_scrobble.scrobble_cache import ScrobbleCache
 import logging
 logging.disable(logging.CRITICAL)
 
+
 class TestScrobbleCache(unittest.TestCase):
 
     def setUp(self):
-        ''' create an empty scrobble_cache object. '''
+        """ create an empty scrobble_cache object. """
         self.sc = ScrobbleCache(config)
 
     def tearDown(self):
-        ''' clean up/remove our temporary cache after test run completes. '''
+        """ clean up/remove our temporary cache after test run completes. """
 
         if os.path.isfile(config.get('plex-scrobble',
             'cache_location')):
@@ -23,11 +24,12 @@ class TestScrobbleCache(unittest.TestCase):
 
     def test_add_record_to_cache(self):
         self.sc.add('artist', 'track', 'album')
+
         self.assertTrue(self.sc.length() == 1)
 
     def test_delete_record_from_cache(self):
         for key in self.sc.cache:
-            sc.remove(key)
+            self.sc.remove(key)
         self.assertTrue(self.sc.length() == 0)
 
 if __name__ == '__main__':
--- tests/test_log_parser.py
+++ tests/test_log_parser.py
@@ -5,21 +5,23 @@ from plex_scrobble.plex_monitor import parse_line
 import logging
 logging.disable(logging.CRITICAL)
 
+
 class TestUnicodeLogParser(unittest.TestCase):
 
     def setUp(self):
-        with  open('data/unicode_pms.log', 'r') as fh:
-            self.found = [ parse_line(line) for line in fh if parse_line(line) ]
-    
+        with open('data/unicode_pms.log', 'r') as fh:
+            self.found = [parse_line(line) for line in fh if parse_line(line)]
+
     def test_unicode_logparser_5_ids(self):
-    
+
         self.assertTrue(len(self.found) == 5)
+
 
 class TestUniversalLogParser(unittest.TestCase):
 
     def setUp(self):
         with open('data/universal_transcode.log', 'r') as fh:
-            self.found = [ parse_line(line) for line in fh if parse_line(line) ]
+            self.found = [parse_line(line) for line in fh if parse_line(line)]
 
     def test_universal_logparser_2_ids(self):
 
